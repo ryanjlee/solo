b1f3b737aee00b3a03434099f51770617525a5dd
Ryan <ryanjlee12@gmail.com>
2015-01-19 17:50:21 -0800

    Partially implement congratulate function

--- app.js
+++ app.js
@@ -25,22 +25,32 @@ angular.module('toDoList', [
     $scope.data.tasks = Tasks.getTasks();
   };
 
-  $scope.deleteTask = function(param){
-    $scope.data.tasks.splice(param, 1);
+  $scope.completeTask = function(index){
+    $scope.data.tasks.splice(index, 1, Tasks.congratulate());
+  };
+
+
+  $scope.deleteTask = function(index){
+    $scope.data.tasks.splice(index, 1);
   };
 
   $scope.fetchTasks();
 })
 
-.factory('Tasks', function () {
+.factory('Tasks', function (){
   var taskList = ['stuff', 'more stuff', 'more other stuff'];
+  var feelGoodQuotes = ['Complete!', 'Good job!', 'Niiiiice!', 'You did it!', 'Awwwwww yeaaaaaaah!', 'You\'re awesome!', 'Holy shit, you\'re killing it!', 'Fuck yeah!', 'Don\'t have to deal with that shit anymore!'];
 
   var getTasks = function(){
     return taskList;
   };
 
-  var addTask = function(tasks) {
+  var addTask = function(tasks){
   	taskList.push(tasks.task);
+  };
+
+  var congratulate = function(){
+    return feelGoodQuotes[Math.random() * feelGoodQuotes.length];
   };
 
   return {
